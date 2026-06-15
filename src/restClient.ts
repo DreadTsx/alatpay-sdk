3c6f6fa4c0623cc9c6cd809cc850951a2ea6a266
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-06-15 19:46:23 +0100

    Implement credentials loading

--- src/restClient.ts
+++ src/restClient.ts
@@ -1,14 +1,66 @@
 import { HTTPMethod } from "./enums.ts";
 
-export default class RestClient {
-  static BASE_URL = "";
+type KeyDisplayNames =
+  | "public key"
+  | "secret key"
+  | "business id"
+  | "webhook secret key";
 
-  async call(endpoint: string, method: HTTPMethod, data?: any, noAuth: boolean = false) {
-    const request = new Request(`${RestClient.BASE_URL}${endpoint}`, {
+export default class RESTClient {
+  static BASE_URL = "https://apibox.alatpay.ng";
+  static ALATPAY_BUSINESS_ID_NAME = "ALATPAY_BUSINESS_ID";
+  static ALATPAY_PUBLIC_KEY_NAME = "ALATPAY_PUBLIC_KEY";
+  static ALATPAY_SECRET_KEY_NAME = "ALATPAY_SECRET_KEY";
+  static ALATPAY_WEBHOOK_SECRET_KEY_NAME = "ALATPAY_WEBHOOK_SECRET_KEY";
+
+  secretKey: string;
+  businessId: string;
+  webhookSecretKey: string;
+  publicKey: string;
+
+  constructor(
+    secretKey?: string,
+    businessId?: string,
+    webhookSecretKey?: string,
+    publicKey?: string,
+  ) {
+    this.loadKey(
+      "secret key",
+      RESTClient.ALATPAY_SECRET_KEY_NAME,
+      secretKey,
+    );
+    loadKey(
+      "business id",
+      RESTClient.ALATPAY_BUSINESS_ID_NAME,
+      businessId,
+      true,
+    );
+    loadKey(
+      "public key",
+      RESTClient.ALATPAY_PUBLIC_KEY_NAME,
+      businessId,
+      true,
+    );
+    loadKey(
+      "webhook secret key",
+      RESTClient.ALATPAY_WEBHOOK_SECRET_KEY_NAME,
+      webhookSecretKey,
+      false,
+    );
+  }
+
+  async call(
+    endpoint: string,
+    method: HTTPMethod,
+    data?: any,
+  ) {
+    const request = new Request(`${RESTClient.BASE_URL}${endpoint}`, {
       method,
       headers: this.secureHeaders,
       body: JSON.stringify(data),
     });
+    const response = await fetch(request);
+    return response.json();
   }
 
   private get baseHeaders() {
@@ -22,7 +74,52 @@ export default class RestClient {
   private get secureHeaders() {
     return {
       ...this.baseHeaders,
-      "Ocp-Apim-Subscription-Key": "",
+      "Ocp-Apim-Subscription-Key": this.secretKey,
     };
+  }
+
+  private loadKey(
+    keyName: KeyDisplayNames,
+    keyEnvName: string,
+    value?: string,
+    deferRequirement: boolean = false,
+  ) {
+    // TODO: Make use of a single switch statement
+    if (value) {
+      switch (keyName) {
+        case "public key":
+          this.publicKey = value;
+          break;
+        case "secret key":
+          this.secretKey = value;
+          break;
+        case "business id":
+          this.businessId = value;
+          break;
+        case "webhook secret key":
+          this.wehookSecretKey = value;
+      }
+      return;
+    }
+    if (!Deno.env.has(keyEnvName) && !deferRequirement) {
+      throw new ClientError(
+        `${keyName} was not provided on instantiation or set in environmental variables as ${keyEnvName}`,
+      );
+    }
+    if (!Deno.env.has(keyEnvName) && deferRequirement) return;
+    const envValue = Deno.env.get(keyEnvName) as string;
+    switch (keyName) {
+      case "public key":
+        this.publicKey = envValue;
+        break;
+      case "secret key":
+        this.secretKey = envValue;
+        break;
+      case "business id":
+        this.businessId = envValue;
+        break;
+      case "webhook secret key":
+        this.webhookSecretKey = envValue;
+    }
   }
 }
