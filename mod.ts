f4239d436b17c62da717239d16fd46d49708888e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-06-15 19:20:09 +0100

    Export public APIs

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+export { default as ALATPayClient } from "./src/alatpay.ts";
+export * from "./src/types/global.ts";
+export * from "./src/types/responseModels.ts";
+export * from "./src/enums.ts";
