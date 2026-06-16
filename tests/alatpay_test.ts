230d412863f3e8135cced62aa926fbfc465d78fd
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-06-16 17:47:47 +0100

    Fix test

--- tests/alatpay_test.ts
+++ tests/alatpay_test.ts
@@ -1,20 +1,34 @@
 import { beforeAll, describe, it } from "@std/testing/bdd";
 import { assertEquals } from "@std/assert";
 import { load } from "@std/dotenv";
-import ALATPayClient from '../src/alatpay.ts'
+import ALATPayClient from "../src/alatpay.ts";
+import { Currency } from "../src/enums.ts";
 
 describe("ALATPayClient", () => {
   let client: ALATPayClient;
 
-beforeAll(async () => {
+  beforeAll(async () => {
     await load({ envPath: ".env", export: true });
     client = new ALATPayClient();
   });
 
-it("generateVirtualAccount", () => {
-    const  payload = {}
-    const response = client.generateVirtualAccount(payload)
+  it("generateVirtualAccount", async () => {
+    const payload = {
+      amount: 100,
+      currency: Currency.NGN,
+      orderId: crypto.randomUUID(),
+      description: "test payin",
+      customer: {
+        email: "adeyigbenga005@gmail.com",
+        phone: "08033221129",
+        firstName: "Gbenga",
+        lastName: "Adeyi",
+        metadata: "",
+      },
+    };
+    const response = await client.generateVirtualAccount(payload);
+    console.log(response);
   });
 
   it.skip("initPayViaUSSD", () => {});
-})
+});
