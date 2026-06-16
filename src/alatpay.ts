2befc3406350ee9f8f2bea97efa812c0bcef30ed
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-06-16 17:48:39 +0100

    Add missing imports and guards

--- src/alatpay.ts
+++ src/alatpay.ts
@@ -1,5 +1,12 @@
 import RESTClient from "./restClient.ts";
 import type { GenerateVirtualAccountPayload } from "./types/global.ts";
+import type {
+  APIErrorResponse,
+  APISuccessResponse,
+  VirtualAccount,
+} from "./types/responseModels.ts";
+import { HTTPMethod } from "./enums.ts";
+import { ClientError } from "./errors.ts";
 
 export default class ALATPayClient {
   client: RESTClient;
@@ -18,13 +25,20 @@ export default class ALATPayClient {
     );
   }
 
-  async generateVirtualAccount(
+  generateVirtualAccount(
     payload: GenerateVirtualAccountPayload,
-  ): Promise<APIResponse<VirtualAccount>> {
+  ): Promise<APISuccessResponse<VirtualAccount> | APIErrorResponse> {
+    const _payload = structuredClone(payload);
+    if (!_payload.businessId && this.client.businessId == "") {
+      throw new ClientError(
+        "businessId not provided in payload, set in environmental variables or provided on client instantiation",
+      );
+    }
+    if (!_payload.businessId) _payload.businessId = this.client.businessId;
     return this.client.call(
       "/bank-transfer/api/v1/bankTransfer/virtualAccount",
       HTTPMethod.POST,
-      payload,
+      _payload,
     );
   }
 
