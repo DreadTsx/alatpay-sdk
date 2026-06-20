422f23db59472f85df989c90544e6083fdc063a3
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-06-20 13:31:18 +0100

    Export errors

--- mod.ts
+++ mod.ts
@@ -2,3 +2,4 @@ export { default as ALATPayClient } from "./src/alatpay.ts";
 export * from "./src/types/global.ts";
 export * from "./src/types/responseModels.ts";
 export * from "./src/enums.ts";
+export * from "./src/errors.ts";
